d4d1b622ce10c87f43e6974e1a78234b58f1a9f4
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2021-12-01 10:57:13 +0900

    Fill out featured box

--- src/pages/News/Featured/index.tsx
+++ src/pages/News/Featured/index.tsx
@@ -4,12 +4,43 @@ import CellHeader from "@/components/CellHeader";
 
 import styles from "./Featured.module.scss";
 
+import data from "./featuredData.json";
+
+function Author({ picture, name }: { picture: string; name: string }) {
+  return (
+    <div className={styles.Author}>
+      <div className={styles.Picture}>{picture}</div>
+      <div className={styles.By}>posted by</div>
+      <div className={styles.Name}>{name}</div>
+    </div>
+  );
+}
+
 function Featured() {
   return (
     <div className={styles.Featured}>
       <CellHeader>Featured</CellHeader>
+      <div className={styles.FeaturedGrid}>
+        <div className={styles.Video}>{data.videoSrc}</div>
+        <div className={styles.Title}>{data.title}</div>
+        <div className={styles.Caption}>{data.caption}</div>
+        <div className={styles.DateLabel}>
+          <span className={styles.Source}>{data.source}</span>
+          <span className={styles.Divider}>|</span>
+          <span className={styles.Date}>{data.date}</span>
+        </div>
+        <Author picture="C" name="Chloe" />
+      </div>
     </div>
   );
 }
 
 export default Featured;
+
+/*<iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/_O0eprmOhwg" 
+    title="YouTube video player" frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; 
+      encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+  >
+  </iframe> */
