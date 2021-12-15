10ed279325a9985429bdfa0e45919aa4f4a1f64e
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2021-12-15 18:03:53 +0900

    Fix products label not translating

--- src/components/Navbar/NavMenu/index.tsx
+++ src/components/Navbar/NavMenu/index.tsx
@@ -6,11 +6,12 @@ import { NAV_ITEMS } from "../NavbarItems";
 import NavLinkButton from "./NavLinkButton";
 import NavSubMenu from "./NavSubMenu";
 import LanguageSelector from "@/components/LanguageSelector";
+import { Trans } from "@lingui/macro";
 
 function NavMenu() {
   return (
     <div className={styles.NavMenu}>
-      <NavSubMenu label="Products" />
+      <NavSubMenu label={<Trans id="Products" />} />
       {NAV_ITEMS.map((item) => (
         <NavLinkButton key={item.id} href={item.path} label={item.label} />
       ))}
